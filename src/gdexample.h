ae035f49055d74559a0a06c199d4b9979f4be898
matpac <matara@ttu.ee>
2023-08-20 22:58:35 +0300

    gdextension example

--- src/gdexample.h
+++ src/gdexample.h
@@ -3,23 +3,33 @@
 
 #include <godot_cpp/classes/sprite2d.hpp>
 
-namespace godot {
+namespace godot
+{
 
-class GDExample : public Sprite2D {
-    GDCLASS(GDExample, Sprite2D)
+    class GDExample : public Sprite2D
+    {
+        GDCLASS(GDExample, Sprite2D)
 
-private:
-    double time_passed;
+    private:
+        double time_passed;
+        double time_emit;
+        double amplitude;
+        double speed;
 
-protected:
-    static void _bind_methods();
+    protected:
+        static void _bind_methods();
 
-public:
-    GDExample();
-    ~GDExample();
+    public:
+        GDExample();
+        ~GDExample();
+        void _process(double delta) override;
 
-    void _process(double delta);
-};
+        void set_amplitude(const double p_amplitude);
+        double get_amplitude() const;
+
+        void set_speed(const double p_speed);
+        double get_speed() const;
+    };
 
 }
 
