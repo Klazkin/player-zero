56330c62aeff194bed3b45aa8e7fb4413bbd5790
matpac <matara@ttu.ee>
2024-02-04 15:50:10 +0200

    integrate bresenhams into surface; los check action; clean up

--- src/main.cpp
+++ src/main.cpp
@@ -13,46 +13,4 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    bresenham(23, 57, -10, -2);
-    printf("\n");
-}
-
-void bresenham(int x1, int y1, int x2, int y2)
-{
-    int dx = abs(x1 - x2); // difference
-    int dy = abs(y1 - y2);
-
-    int tx = (x1 < x2) ? 1 : -1; // direction of change (positive or negative)
-    int ty = (y1 < y2) ? 1 : -1;
-
-    if (dx >= dy) //  dx > dy -> x increases faster (easy slope)
-    {
-        int pk = 2 * dy - dx;
-        for (int x = x1, y = y1; x != x2 + tx; x += tx)
-        {
-            cout << "(" << x << "," << y << "), ";
-
-            if (pk >= 0)
-            {
-                y += ty;
-                pk -= 2 * dx;
-            }
-            pk += 2 * dy;
-        }
-    }
-    else // y increases faster (steep slope)
-    {
-        int pk = 2 * dx - dy;
-        for (int x = x1, y = y1; y != y2 + ty; y += ty)
-        {
-            cout << "(" << x << "," << y << "), ";
-
-            if (pk >= 0)
-            {
-                x += tx;
-                pk -= 2 * dy;
-            }
-            pk += 2 * dx;
-        }
-    }
 }
--- src/main.h
+++ src/main.h
@@ -2,6 +2,5 @@
 #define MAIN_H
 
 int main(int argc, char const *argv[]);
-void bresenham(int x1, int y1, int x2, int y2);
 
 #endif
